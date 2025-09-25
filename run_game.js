7f426515ee7ffb25677d6f1393baeba6151ab8bd
Michael Kaszynski <mkaszynski2000@gmail.com>
2025-09-25 13:32:35 -0600

    restart when crashed, and change platforms

--- run_game.js
+++ run_game.js
@@ -4,9 +4,13 @@ canvas.height = 600;
 document.body.appendChild(canvas);
 const ctx = canvas.getContext("2d");
 
+let length = 400;
+
+let explosions = [];
+
 let color = [Math.random()*255, Math.random()*255, Math.random()*255];
 
-let platforms = [[10, 0, color.slice()], [408, 0, color.slice()], [806, 0, color.slice()], [1204, 0, color.slice()]];
+let platforms = [[10, 0, color.slice(), 400], [408, 0, color.slice(), 400], [806, 0, color.slice(), 400], [1204, 0, color.slice(), 400]];
 
 let background = [];
 
@@ -15,10 +19,11 @@ let posy = 0;
 
 let vely = 0;
 
+let height = 0;
+
+
 let keys = {};
 let mouse = { x: 0, y: 0, held: [false, false, false] };
-
-let height = 0;
 
 function dis(pos1, pos2) {
   const x = (pos2[0] - pos1[0]) ** 2;
@@ -26,8 +31,8 @@ function dis(pos1, pos2) {
   return Math.sqrt(x + y);
 }
 
-function draw_circle(x, y, radius, color) {
-  ctx.fillStyle = color;        // color
+function draw_circle(x, y, radius, color1) {
+  ctx.fillStyle = color1;        // color
   ctx.beginPath();              // start a new path
   ctx.arc(x, y, radius, 0, Math.PI * 2); // x, y, radius, startAngle, endAngle
   ctx.fill();
@@ -66,9 +71,31 @@ canvas.addEventListener("touchend", e => {
 canvas.addEventListener("mousedown", e => mouse.held[e.button] = true);
 canvas.addEventListener("mouseup", e => mouse.held[e.button] = false);
 
+let start = true;
+
 let running = true;
 function loop() {
   if (!running) return;
+
+  if (start) {
+    length = 400;
+
+    explosions = [];
+
+    color = [Math.random()*255, Math.random()*255, Math.random()*255];
+
+    platforms = [[10, 0, color.slice(), 400], [408, 0, color.slice(), 400], [806, 0, color.slice(), 400], [1204, 0, color.slice(), 400]];
+
+    background = [];
+
+    posx = 0;
+    posy = 0;
+
+    vely = 0;
+
+    height = 0;
+    start = false;
+  }
 
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -83,6 +110,17 @@ function loop() {
     color[i] = Math.max(0, color[i]);
   }
 
+  if (explosions.length > 0) {
+    for (let i = 0; i < explosions.length; i++) {
+      explosions[i][0] += explosions[i][2];
+      explosions[i][1] += explosions[i][3];
+      explosions[i][3] += 0.5;
+      if (explosions[i][1] > 10000) {
+        start = true;
+      }
+    }
+  }
+
   if (Math.random() > 0.95) {
     let a = Math.random()*100;
     background.push([1200, Math.random()*600 + posy/a, color.slice(), a]);
@@ -90,27 +128,30 @@ function loop() {
 
   if (background.length > 0) {
     for (let i = 0; i < background.length; i++) {
-      background[i][0] -= background[i][3]/20*(posx/5000 + 4)/5;
+      if (explosions.length === 0) background[i][0] -= background[i][3]/20*(posx/5000 + 4)/5;
       if (background[i][0] < -i[3]) {
         background.splice(i, 1);
       }
     }
   }
 
-  posx += posx/5000 + 4;
-  posy += vely;
+  if (explosions.length === 0) posx += posx/5000 + 4;
+  if (explosions.length === 0) posy += vely;
 
   vely += 0.5;
 
   let n = true;
 
   for (let i of platforms) {
-    if (i[0] > posx + 1200) {
+    if (i[0] + length + i[3] > posx + 1200) {
       n = false;
     }
-    if (i[0] < posx && posx < i[0] + 400 && posy > i[1]) {
+    if (i[0] < posx && posx < i[0] + i[3] && posy > i[1] && explosions.length === 0) {
       if (posy > i[1] + 10) {
-        running = false;
+        for (let i = 0; i < 50; i++) {
+          let angle = Math.random()*100;
+          explosions.push([600, 400, Math.sin(angle)*Math.random()*15 + posx/5000 + 4, Math.cos(angle)*Math.random()*15])
+        }
       } else {
         posy = i[1];
         vely = 0;
@@ -129,8 +170,9 @@ function loop() {
   }
 
   if (n) {
-    height += Math.random()*160 - 80
-    platforms.push([posx + 1600 - posx/5000 - 7, height, color.slice()]);
+    height += Math.random()*150 - 75
+    platforms.push([posx - length + 1200 - 15, height, color.slice(), length]);
+    length = Math.random()*600;
   }
 
   for (let i of background) {
@@ -140,11 +182,18 @@ function loop() {
 
   for (let i of platforms) {
     ctx.fillStyle = "rgb(" + i[2][0] + "," + i[2][1] + "," + i[2][2] + ")";
-    ctx.fillRect(i[0] - posx + 600, i[1] - posy + 400, 400, 600);
+    ctx.fillRect(i[0] - posx + 600, i[1] - posy + 400, i[3], 1200);
   };
 
-  ctx.fillStyle = "rgb(255, 0, 0)";
-  ctx.fillRect(580, 380, 20, 20);
+  for (let i of explosions) {
+    ctx.fillStyle = "rgb(255, 0, 0)";
+    ctx.fillRect(i[0], i[1], 10, 10);
+  };
+
+  if (explosions.length === 0) {
+    ctx.fillStyle = "rgb(255, 0, 0)";
+    ctx.fillRect(580, 380, 20, 20);
+  }
 
   ctx.fillStyle = "white";          // text color
   ctx.font = "30px Arial";          // font size and family
